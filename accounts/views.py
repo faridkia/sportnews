a588a7f8487642f488d7ef94e5832de8f4256aa6
faridkia <faridkiaeieh@gmail.com>
2024-08-27 11:35:23 +0330

    add forms.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, HttpResponse
 from .forms import SignUpForm
-
-# Create your views here.
+import sqlite3
+from .models import User
 def SignUp(request):
     if request.method == 'POST':
-        return HttpResponse(f'Salam')
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(username=cd['username'], password=cd['password'])
+            return HttpResponse('User Sakhte Shod')
+        else:
+            ...
     else:
         form = SignUpForm()
         return render(request, 'accounts/signup.html', context={'form':form})
+
+def insert(request):
+    form = SignUpForm(request.POST)
+
+    query = "insert into table users(username, password) values ()"
